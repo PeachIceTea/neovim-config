455e36bc8c16c4218f6e859c5b66d5b356237e36
Jonas Thiem <jonthiem@gmail.com>
2026-03-20 13:39:22 +0100

    add mass buffer deletion >:)

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,6 +11,25 @@ return {
 			vim.keymap.set("n", "<leader>c", function()
 				require("mini.bufremove").delete()
 			end, { desc = "Close buffer" })
+
+			vim.keymap.set("n", "<leader>bc", function()
+				local bufremove = require("mini.bufremove")
+				for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+					if vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted then
+						bufremove.delete(buf, false)
+					end
+				end
+			end, { desc = "Close all buffers" })
+
+			vim.keymap.set("n", "<leader>bo", function()
+				local bufremove = require("mini.bufremove")
+				local current = vim.api.nvim_get_current_buf()
+				for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+					if vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and buf ~= current then
+						bufremove.delete(buf, false)
+					end
+				end
+			end, { desc = "Close all other buffers" })
 		end,
 	},
 }
