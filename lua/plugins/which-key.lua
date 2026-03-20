f43062a330ce9c7cd483daa628c775a077a1ef13
Jonas Thiem <jonthiem@gmail.com>
2026-03-20 13:39:39 +0100

    add missing <leader>f description

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -7,6 +7,7 @@ return {
 				{ "<leader>m", group = "messages" },
 				{ "<leader>b", group = "buffers" },
 				{ "<leader>t", group = "terminal" },
+				{ "<leader>f", group = "find" },
 				{ "<leader>l", group = "lsp" },
 				{ "<leader>g", group = "git" },
 			},
