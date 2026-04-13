d56b23e89c73baff3a8a240ad11e4f82dd3925a9
Jonas Thiem <jonthiem@gmail.com>
2026-04-13 12:21:45 +0200

    Tweak indent-blankline colors: faint gray lines with gruvbox orange scope highlight

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -5,13 +5,18 @@ return {
 		indent = {
 			char = "│",
 			tab_char = "│",
-			highlight = "Whitespace",
+			highlight = "ibl_indent",
 		},
 		scope = {
 			enabled = true,
 			show_start = false,
 			show_end = false,
-			highlight = "Comment",
+			highlight = "ibl_scope",
 		},
 	},
+	config = function(_, opts)
+		vim.api.nvim_set_hl(0, "ibl_indent", { fg = "#928374" })
+		vim.api.nvim_set_hl(0, "ibl_scope", { fg = "#fe8019" })
+		require("ibl").setup(opts)
+	end,
 }
